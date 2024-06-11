bfc32327593d526647b5db6932d3359a810d39bd
jk <johnkraus3@gmail.com>
2024-06-11 13:42:33 -0400

    created summaries

--- data/downloaded_request_summaries/download_url.py
+++ data/downloaded_request_summaries/download_url.py
@@ -3,6 +3,10 @@ from urllib.request import urlretrieve
 import requests
 import csv
 import os
+from bs4 import BeautifulSoup
+import sys
+import re
+
 
 # https://realpython.com/python-download-file-from-url/
 
@@ -44,12 +48,60 @@ for row in link_data:
     # print(response.headers)
     # print("response_url = ", response.url)
     # print("response.status_code = ", response.status_code)
-    print("response.content = ", response.text)
+    # print("response.text = ", response.text)
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    if soup.find('span', class_="submitted"): # .decompose()
+        soup.find('span', class_="submitted").decompose()
+    if soup.find('footer'):  # .decompose()
+        soup.find('footer').decompose()
+
+    if soup.find('div', class_="field-name-field-reference-number"):  # .decompose()
+        soup.find('div', class_="field-name-field-reference-number").decompose()
+
+    claz = "field-name-field-name"
+    if soup.find('div', class_=claz):  # .decompose()
+        soup.find('div', class_= claz).decompose()
+
+    claz = "field-name-field-legal-basis"
+    if soup.find('div', class_=claz):  # .decompose()
+        soup.find('div', class_= claz).decompose()
+
+    claz = "field-name-field-posted-on"
+    if soup.find('div', class_=claz):  # .decompose()
+        soup.find('div', class_= claz).decompose()
+
+    claz = "field-name-field-updated-on"
+    if soup.find('div', class_=claz):  # .decompose()
+        soup.find('div', class_= claz).decompose()
+
+    strongs = soup.find_all('strong')
+    for strong in strongs:
+        t = strong.text
+        soup.find('strong').replace_with(t)
+
+
+    # addtl_info = soup.find('div', class_="field-name-field-additional-information").prettify()
+    region_content = soup.find('div', class_="region-content").prettify()
+
+
+    # region_content.find('span', class_="submitted").decompose()
+    # region_content.submitted.decompose()
+
+
+    print(region_content)
+    # exit()
+
+    # node-1461 > div.field.field-name-field-additional-information.field-type-text-long.field-label-above
+
+
+
 
     # for k in response.headers.keys(): # .items():
     #      print(k, ": ", response.headers[k])
     filepath = "/Users/john.kraus/workspaces/aq-list-viz/data/downloaded_request_summaries/" + filename
     # with open("/Users/john.kraus/workspaces/aq-list-viz/data/downloaded_request_summaries/testfile.html", mode="w") as file:
     with open(filepath, mode="w") as file:
-        file.write(str(response.text))
+        file.write(str(region_content))
         file.close()
+
