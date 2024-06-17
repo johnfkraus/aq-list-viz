7c52affbd791b5c77876acd294c522b53cfea4f5
jk <johnkraus3@gmail.com>
2024-06-17 09:28:09 -0400

    added png icon for closing info box 2

--- js/Document.js
+++ js/Document.js
@@ -9,24 +9,25 @@ function Document() { // }, width) {
     docContainer,
     topStuffHeight = $('#top-stuff').height();
 
-//  this.elementId = elementId;
-//  console.log('Document.js topStuffHeight = ', topStuffHeight);
+    //  this.elementId = elementId;
+    //  console.log('Document.js topStuffHeight = ', topStuffHeight);
 
   hideDocument();
 
   var showDocument = function (d, content, event) {
-//  var showDocument = function (content, event, d) {
+    //  var showDocument = function (content, event, d) {
     var that = this;
     // elementId is 'viz-doc'
+    console.log("22 hello from Document.js showDocument()");
+
     $('span#name').html('Document.js' + d.name);
     $('span#id').html(d.id);
     $('span#nameOriginalScript').html(d.NAME_ORIGINAL_SCRIPT);
-    // $('span#narrative').html(d.COMMENTS1);
+    $('span#narrative').html(d.COMMENTS1);
     if (d.indiv0OrEnt1 == 0 && d.INDIVIDUAL_DATE_OF_BIRTH !== 'undefined') {
       $('span#indivDob').html(d.INDIVIDUAL_DATE_OF_BIRTH);
-
     } else {
-//   $('div#dateOfBirthDiv').css('display', 'none');
+      $('div#dateOfBirthDiv').css('display', 'none');
     }
 
     $('#doc-container').show();
@@ -36,6 +37,7 @@ function Document() { // }, width) {
   };
 
   function hideDocument() {
+    console.log("41 hello from Document.js hideDocument()");
     $('#doc-close').css('display', 'none');
 //    $('#doc-close').hide();
     $('#doc-container').hide();
